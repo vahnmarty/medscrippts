78e017364df01a9d45331ee0a3e2cbbf051743bc
vahnmarty <vahnmarty@gmail.com>
2023-03-29 02:22:29 +0800

    dashboard test

--- tests/Feature/UserDashboardTest.php
+++ tests/Feature/UserDashboardTest.php
@@ -1,7 +1,25 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\User;
+use App\Http\Controllers\DashboardController;
 
+test('can see dashboard page & can view scripts', function () {
+    $user = User::first();
+    
+    // Login as the test user
+    $this->actingAs($user);
+    
+    // Make a GET request to the /dashboard route
+    $response = $this->get('/dashboard');
+    
+    // Assert that the response status code is 200 (OK)
     $response->assertStatus(200);
+    
+    $controller = new DashboardController();
+    
+    // Call the index() method to generate the $scripts variable
+    $view = $controller->index();
+    $scriptsVariable = $view->getData()['scripts'];
+    
+    $this->assertGreaterThan(1, count($scriptsVariable));
 });
