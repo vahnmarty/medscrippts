6e2f608a990c1bb68eb063520476f0455fecf58e
vahnmarty <vahnmarty@gmail.com>
2023-04-20 00:55:15 +0800

    horizon

--- config/app.php
+++ config/app.php
@@ -193,6 +193,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         \SocialiteProviders\Manager\ServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
