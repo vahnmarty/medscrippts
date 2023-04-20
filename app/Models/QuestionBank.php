b23916f40d518b619a55c519a55abf877363783f
vahnmarty <vahnmarty@gmail.com>
2023-04-21 05:01:21 +0800

    stats

--- app/Models/QuestionBank.php
+++ app/Models/QuestionBank.php
@@ -25,4 +25,46 @@ class QuestionBank extends Model
     {
         return $this->hasMany(QuestionBankRecord::class);
     }
+
+    public static function stats()
+    {
+        $total = 0;
+        $success = 0;
+        $invalid_option_answer = 0;
+        $invalid_answer = 0;
+
+        foreach(self::get() as $item)
+        {
+            $total++;
+
+            $options = ['option1', 'option2', 'option3', 'option4'];
+
+            if( in_array($item->option_answer, $options)){
+                $success++;
+            }else{
+                $invalid_option_answer++;
+            }
+
+            $correct = false;
+            foreach($options as $opt)
+            {   
+                if($item->$opt == $item->answer )
+                {
+                    $correct = true;
+                    break;
+                }
+            }
+
+            if(!$correct){
+                $invalid_answer++;
+            }
+        }
+
+        return [
+            'total' => $total,
+            'success' => $success,
+            'invalid_option_answer' => $invalid_option_answer,
+            'invalid_answer' => $invalid_answer
+        ];
+    }
 }
