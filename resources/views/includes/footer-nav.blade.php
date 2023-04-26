70e3d5c26080c3a30bc2480b23f748651134fbfb
vahnmarty <vahnmarty@gmail.com>
2023-04-27 05:54:12 +0800

    footer

--- resources/views/includes/footer-nav.blade.php
+++ resources/views/includes/footer-nav.blade.php
@@ -1,18 +1,18 @@
 <div class="fixed bottom-0 left-0 right-0 bg-white border-t">
     <div class="flex justify-evenly">
-        <a href="" class="px-2 py-2">
+        <a href="" class="px-3 py-3">
             <x-heroicon-o-home class="w-8 h-8 text-gray-600"/>
         </a>
-        <a href="" class="px-2 py-2">
+        <a href="" class="px-3 py-3">
             <x-heroicon-o-clipboard-list class="w-8 h-8 text-gray-600"/>
         </a>
-        <a href="" class="px-2 py-2">
+        <a href="" class="px-3 py-3">
             <x-heroicon-o-view-grid class="w-8 h-8 text-gray-600"/>
         </a>
-        <a href="" class="px-2 py-2">
+        <a href="" class="px-3 py-3">
             <x-heroicon-o-view-boards class="w-8 h-8 text-gray-600"/>
         </a>
-        <a href="" class="px-2 py-2">
+        <a href="" class="px-3 py-3">
             <x-heroicon-o-filter class="w-8 h-8 text-gray-600"/>
         </a>
     </div>
