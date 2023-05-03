5af5c724a05933b8645ff4889732731d6e7ddf1e
vahnmarty <vahnmarty@gmail.com>
2023-05-04 06:04:42 +0800

    qbank UI

--- app/Http/Livewire/QuestionBank/ManageQuestionBanks.php
+++ app/Http/Livewire/QuestionBank/ManageQuestionBanks.php
@@ -4,11 +4,23 @@ namespace App\Http\Livewire\QuestionBank;
 
 use Auth;
 use Livewire\Component;
+use App\Models\Category;
 use Jenssegers\Agent\Agent;
+use App\Models\QuestionBank;
+use App\Models\QuestionBankRecord;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
 
-class ManageQuestionBanks extends Component
+class ManageQuestionBanks extends Component implements HasForms
 {
+    use InteractsWithForms;
+
+    public $qbanks = [];
     
+    public $categories = [], $max= 20;
+
     public function render()
     {
         return view('livewire.question-bank.manage-question-banks');
@@ -16,12 +28,34 @@ class ManageQuestionBanks extends Component
 
     public function mount()
     {
-        $agent = new Agent();
-
-        if($agent->isMobile())
-        {
-            return redirect('qbanks/mobile');
-        }
+        $this->qbanks = QuestionBankRecord::with('categories')->withCount('items')->where('user_id', auth()->id())->get();
     }
-    
+
+    public function getFormSchema()
+    {
+        return [
+            Select::make('categories')
+                ->required()
+                ->multiple()
+                ->options(Category::has('flashcards')->get()->pluck('name', 'id')),
+            TextInput::make('max')
+                ->maxValue(30)
+                ->numeric()
+                ->disabled()
+                ->required()
+        ];
+    }
+
+    public function save()
+    {
+        $data = $this->form->getState();
+        
+        $record = QuestionBankRecord::create(['user_id' => Auth::id()]);
+        $record->categories()->attach($data['categories']);
+        $ids = QuestionBank::whereIn('category_id', $data['categories'])->inRandomOrder()->limit($data['max'])->pluck('id')->toArray();
+        $record->items()->attach($ids);
+
+        return redirect()->route('qbank.play', $record->id);
+    }
+
 }
--- resources/views/livewire/question-bank/manage-question-banks.blade.php
+++ resources/views/livewire/question-bank/manage-question-banks.blade.php
@@ -1,22 +1,72 @@
-<div>
+<div class="bg-gray-100">
 
-    <header class="flex justify-between px-16 py-6">
-        <h1 class="text-4xl font-bold leading-7 text-darkgreen sm:leading-9">Question Banks</h1>
-        <div class="flex gap-6">
-            
+    <x-modal-sm ref="create">
+        <div>
+            <div>
+                <div class="flex">
+                    <x-heroicon-s-book-open class="flex-shrink-0 w-6 h-6 text-gray-500 lg:h-8 lg:w-8" />
+                    <div class="ml-2">
+                        <h3 class="text-lg text-darkgreen">Create QBank</h3>
+                    </div>
+                </div>
+                <p class="mt-2 text-sm text-gray-700">Select categories then input the number of cards you want to
+                    generate.</p>
+
+            </div>
+
+            <form wire:submit.prevent="save" class="mt-8">
+                {{ $this->form }}
+
+                <button type="submit" class="mt-8 btn-primary">Submit</button>
+            </form>
+        </div>
+    </x-modal-sm>
+
+
+    <header class="flex justify-between px-6 py-3 bg-white lg:px-16 lg:py-6">
+        <h1 class="text-2xl font-bold leading-7 text-darkgreen sm:leading-9 lg:text-4xl">Question Banks</h1>
+        <div>
+            <button x-data x-on:click="$dispatch('openmodal-create')" type="button" class="btn-primary">Create</button>
         </div>
     </header>
-    <div class="px-8 py-6 space-y-8 bg-gray-100 lg:px-16">
 
+    <div class="px-6 py-12 space-y-8 bg-gray-100 lg:px-16">
+        <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+            @foreach ($qbanks as $card)
+                <a href="{{ route('qbank.play', $card->id) }}" wire:key="card-{{ $card->id }}"
+                    class="relative block p-5 border border-gray-300 rounded-md bg-gray-50 hover:bg-white">
+                    <h4 class="text-xl font-bold text-orange-600">{{ $card->getCategoriesName() }}</h4>
 
-        <div x-data="{ tab: 'records' }">
+                    <div class="mt-4 space-y-2">
+                        <div class="flex items-center gap-2 text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                            </svg>
+                            <span>{{ $card->items_count ?? 0 }} cards</span>
+                        </div>
+                        <div class="flex items-center gap-2 text-sm text-gray-700">
+                            <x-heroicon-o-calendar class="flex-shrink-0 w-6 h-6" />
+                            <span>{{ $card->created_at->format('F d Y') }}</span>
+                        </div>
+                        <div class="flex items-center gap-2 text-gray-700 {{ $card->score }}">
+                            <x-heroicon-o-star class="flex-shrink-0 w-6 h-6" />
+                            @if($card->score)
+                            <span>{{ $card->score  . '/' . $card->items_count}} </span>
+                            @else
+                            <span>-</span>
+                            @endif
+                        </div>
+                    </div>
 
-            <div class="mt-8">
-                <div x-show="tab == 'records'">
-                    @livewire('question-bank.question-bank-records')
-                </div>
-            </div>
+                    <div class="absolute top-0 bottom-0 right-0 pr-7">
+                        <div class="flex flex-col items-center w-full h-full">
+                            <x-heroicon-o-chevron-right class="flex-1 w-10 h-10 text-gray-300 hover:text-darkgreen" />
+                        </div>
+                    </div>
+                </a>
+            @endforeach
         </div>
-
     </div>
 </div>
