7de8f4f9f44ebd855d58bdfd0015ee9c852fb976
vahnmarty <vahnmarty@gmail.com>
2023-05-04 05:18:56 +0800

    flipup

--- resources/views/includes/pagination/custom-paginator.blade.php
+++ resources/views/includes/pagination/custom-paginator.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data>
 
     @if ($paginator->hasPages())
         <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-center">
@@ -18,7 +18,7 @@
             <span>
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
-                    <button wire:click="nextPage" wire:loading.attr="disabled" rel="next" class="relative inline-flex items-center w-10 h-10 p-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-full hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700">
+                    <button wire:click="nextPage" x-on:click="$dispatch('flipup'); console.log('flip')" wire:loading.attr="disabled" rel="next" class="relative inline-flex items-center w-10 h-10 p-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-full hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700">
                         <x-heroicon-s-arrow-right class="w-6 h-6"/>
                     </button>
                 @else
