93cd194dcc752efca7b631ae2c8f4b1362f51af5
vahnmarty <vahnmarty@gmail.com>
2023-05-06 07:36:56 +0800

    transform 3d

--- resources/views/livewire/home-scripts.blade.php
+++ resources/views/livewire/home-scripts.blade.php
@@ -77,8 +77,8 @@
                                     <div x-data="{ flip: false }" x-on:flipup.window="flip = false"
                                         x-on:flipback.window="flip = true"
                                         class="group h-[32rem] w-full [perspective:1000px]">
-                                        <div :class="flip ? '[transform:rotateY(180deg)]' : ''"
-                                            class="relative h-full w-full rounded-xl transition-all duration-500 [transform-style:preserve-3d]">
+                                        <div :class="flip ? 'rotate-y-180' : ''"
+                                            class="relative w-full h-full transition-all duration-500 rounded-xl transform-3d">
                                             <div class="absolute inset-0 p-4 pb-20 overflow-auto bg-white">
                                                 <div>
                                                     <header class="text-left">
