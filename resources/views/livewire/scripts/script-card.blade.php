db02dea66224e6ad6b1f6bca93f6a29a5a5d0648
vahnmarty <vahnmarty@gmail.com>
2023-05-12 13:35:11 +0800

    loading

--- resources/views/livewire/scripts/script-card.blade.php
+++ resources/views/livewire/scripts/script-card.blade.php
@@ -139,7 +139,23 @@
                             @endforelse
                         </div>
 
-                        <input x-show="edit"  type="file" wire:model="images" multiple>
+                       
+
+                        <div
+                            x-data="{ isUploading: false, progress: 0 }"
+                            x-on:livewire-upload-start="isUploading = true"
+                            x-on:livewire-upload-finish="isUploading = false"
+                            x-on:livewire-upload-error="isUploading = false"
+                            x-on:livewire-upload-progress="progress = $event.detail.progress"
+                        >
+                            <!-- File Input -->
+                            <input x-show="edit" type="file" wire:model="images" multiple>
+                        
+                            <!-- Progress Bar -->
+                            <div x-show="isUploading">
+                                <progress max="100" x-bind:value="progress"></progress>
+                            </div>
+                        </div>
 
                         <x-validation-errors/>
                     </div>
